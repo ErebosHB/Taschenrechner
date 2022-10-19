0e7fbcbf3fa3c6433acfd0d21076489eb003207a
SchroederY <yannick.s3@schule.bremen.de>
2022-10-19 10:14:55 +0200

    first commit

---
+++ taschenrechner.js
@@ -0,0 +1,96 @@
+const ANZEIGE_ID = "anzeige";
+const EINGABEFELD_ID ="eingabefeld";
+const RECHENWEG_ANZEIGE_ID = "rechenweg-anzeige";
+const RECHEN_BUTTON_ID = "rechen-button";
+const TASTE_KLASSE = "taste";
+const FEHLER_KLASSE = "fehler";
+
+const LEERZEICHEN_ERFORDERLICH_REGEX = /(\d|\))\s*(\+|\-|\*|\/)\s*/g;
+const GUELTIGE_EINGABE_REGEX =/^[\d., \(\)\+\-\*\/]*$/;
+
+const anzeige = document.getElementById(ANZEIGE_ID);
+const eingabefeld = document.getElementById(EINGABEFELD_ID);
+const rechenwegAnzeige = document.getElementById(RECHENWEG_ANZEIGE_ID);
+const rechenButton = document.getElementById(RECHEN_BUTTON_ID);
+const tasten = document.getElementsByClassName(TASTE_KLASSE);
+
+const formatierer = new Intl.NumberFormat(undefined, {
+    maximumFractionDigits:15,
+    useGrouping: false,
+});
+
+let aktuelleEingabe = "";
+
+for(const taste of tasten){
+    taste.addEventListener("click", function(ereignis){
+
+        rechenwegAnzeige.textContent = "";
+
+        const angeklicktesZeichen = ereignis.target.textContent;
+
+        switch(angeklicktesZeichen){
+            
+            case "ENTF":
+                eingabefeld.value = eingabefeld.value.trimEnd().slice(0, -1).trimEnd();
+                
+                aktuelleEingabe = eingabefeld.value;
+                break;
+
+            case"AC":
+            eingabefeld.value = "";
+            aktuelleEingabe = "";
+            break;
+
+            
+                
+                break;
+            case "=":
+                try{
+                    const ergebnis = new Function("return " + eingabefeld.value.replaceAll(",","."))();
+
+                    eingabefeld.value = formatierer.format(ergebnis);
+
+                    rechenwegAnzeige.textContent = aktuelleEingabe;
+
+                    aktuelleEingabe = eingabefeld.value;
+
+                    rechenwegAnzeige.classList.remove(FEHLER_KLASSE)
+
+                } catch{
+                rechenwegAnzeige.textContent = "Ungültige Eingabe!"
+                rechenwegAnzeige.classList.add(FEHLER_KLASSE)
+            }
+            break;
+            
+            
+            default:
+                eingabefeld.value += angeklicktesZeichen;
+
+                eingabefeld.value = eingabefeld.value.replaceAll(LEERZEICHEN_ERFORDERLICH_REGEX, "$1 $2 ");
+                
+                aktuelleEingabe = eingabefeld.value;
+            
+        }
+
+        
+
+    });
+}
+
+eingabefeld.addEventListener("input", function(ereignis){
+    if (GUELTIGE_EINGABE_REGEX.test(eingabefeld.value)){
+        aktuelleEingabe = eingabefeld.value;
+    }
+    else{
+        eingabefeld.value = aktuelleEingabe;
+
+    }
+
+});
+
+eingabefeld.addEventListener("keydown", function(ereignis){
+    if(ereignis.key ==="Enter") {
+        rechenButton.click();
+    }
+});
+
